53d428e1482e3c76789cf69eb18392054bb3f995
idonotmatch <161400056+idonotmatch@users.noreply.github.com>
2024-09-11 16:08:59 -0400

    update login state

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,15 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import ReactGA4 from 'react-ga4';
 import { useRouter } from 'next/router';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import '../styles/globals.css'; // Ensure this path is correct
-import { SearchProvider } from '../context/SearchContext'; // Ensure this path is correct
-import { ListProvider } from '../context/ListContext'; // Ensure this path is correct
-import Layout from '../components/Layout'; // Ensure this path is correct
+import '../styles/globals.css';
+import { SearchProvider } from '../context/SearchContext';
+import { ListProvider } from '../context/ListContext';
+import Layout from '../components/Layout';
 
 // Setup Apollo Client
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: '/api/graphql', // Ensure this endpoint is correct
+    uri: '/api/graphql',
   }),
   cache: new InMemoryCache(),
 });
@@ -21,7 +21,6 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    // Initialize GA4 with the ID from environment variables
     const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
     if (gaMeasurementId) {
       ReactGA4.initialize(gaMeasurementId);
@@ -35,14 +34,13 @@ function MyApp({ Component, pageProps }) {
 
     router.events.on('routeChangeComplete', handleRouteChange);
 
-    // Cleanup subscription on unmount
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange);
     };
   }, [router.events]);
 
   return (
-    <UserProvider>
+    <UserProvider skipInitialCheck>
       <ApolloProvider client={client}>
         <SearchProvider>
           <ListProvider>
--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useRouter } from 'next/router';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 const Login = () => {
-  const { user, error, isLoading } = useUser();
+  const { user, error } = useUser();
   const router = useRouter();
 
-  if (isLoading) return <div>Loading...</div>;
+  useEffect(() => {
+    if (user) {
+      router.push('/');
+    }
+  }, [user, router]);
+
   if (error) return <div>{error.message}</div>;
-  
-  if (user) {
-    router.push('/');
-    return null;
-  }
 
   return (
     <>
