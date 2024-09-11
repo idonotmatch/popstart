3e4fdccc2012f21bd1f4e6ff0744365d45f41d3f
idonotmatch <161400056+idonotmatch@users.noreply.github.com>
2024-09-11 13:24:38 -0400

    temp auth0 troubleshoot

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,12 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res);
+    } catch (error) {
+      console.error('Login error:', error);
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+});
